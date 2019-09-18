33f7540dbeff663b4262c5b0f86c28a95de18fd0
112Aug <leon1583@bears.unco.edu>
2019-09-18 16:20:30 -0600

    Project 3- Almost!
    Works, I just need to add pictures.

--- bacs350/superhero/index.php
+++ bacs350/superhero/index.php
@@ -10,18 +10,18 @@
 
     // Set custom settings
     $site_title = 'UNC BACS 350';
-    $page_title = 'Calebs Superhero Gallery';
+    $page_title = 'Caleb Superhero Gallery';
 
     $card1 = render_card("Batman", "The strong hero in Gotham City");
     $card2 = render_card("Superman", "Superhero of the world from Krypton");
 
-    //$card3 = render_card("Spiderman", "Can get tangled up, but finds a way out")
-    //$card4 = render_card("Iron Man", "Fe - arless hero. Oaky, I'll leave now")
+    $card3 = render_card("Spiderman", "Can get tangled up, but finds a way out");
+    $card4 = render_card("Iron Man", "Fe - arless hero. Okay, Ill leave now");
 
     $content =  '
         <div class="container-fluid">
             <div class="row">
-                ' . $card1 . $card2 . '
+                ' . $card1 . $card2 . $card3 . $card4 .'
             </div>
         </div>
     ';
@@ -30,4 +30,6 @@
     // Create HTML and output the page
     echo render_page($site_title, $page_title, $content);
 
+
+// ' . $card1 . $card2 . $card3 . $card4 '
 ?>
