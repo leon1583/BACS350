95a46c0b2c70c1e32f31e99e221490bf6a1b7985
112Aug <leon1583@bears.unco.edu>
2019-09-18 17:21:02 -0600

    Adding More Cards
    Added more heros to gallery

--- bacs350/superhero/index.php
+++ bacs350/superhero/index.php
@@ -9,19 +9,22 @@
 
 
     // Set custom settings
-    $site_title = 'UNC BACS 350';
-    $page_title = 'Caleb Superhero Gallery';
+    $site_title = 'Caleb\'s Superheros:';
+    $page_title = 'The Superhero Gallery of Caleb Leonard';
 
     $card1 = render_card("Batman", "The strong hero in Gotham City");
     $card2 = render_card("Superman", "Superhero of the world from Krypton");
 
     $card3 = render_card("Spiderman", "Can get tangled up, but finds a way out");
-    $card4 = render_card("Iron Man", "Fe - arless hero. Okay, Ill leave now");
+    $card4 = render_card("Iron Man", "Fe - arless hero. Okay, I'll leave now");
+
+    $card5 = render_card("Bibleman", "A superhero of a cheesy TV show I vaugely remember watching as a kid. Honestly, I never liked it...");
+    $card6 = render_card("Hezekiah", "A king of Judah who brought healing and revival to the country of Israel. Read about him in 2 Chronicles 29-32");
 
     $content =  '
         <div class="container-fluid">
             <div class="row">
-                ' . $card1 . $card2 . $card3 . $card4 .'
+                ' . $card1 . $card2 . $card3 . $card4 . $card5 . $card6 . '
             </div>
         </div>
     ';
